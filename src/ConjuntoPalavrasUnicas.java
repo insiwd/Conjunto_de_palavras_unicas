57371273aa545959e87de744c2c90b1f46f2b063
ruan <ruan.cavalcante505@gmail.com>
2024-05-16 09:50:05 -0300

    feat: verficar palavras

--- src/ConjuntoPalavrasUnicas.java
+++ src/ConjuntoPalavrasUnicas.java
@@ -23,30 +23,10 @@ public class ConjuntoPalavrasUnicas {
     palavasUnicas.remove(palavrasParaRemover);
   }
 
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((palavasUnicas == null) ? 0 : palavasUnicas.hashCode());
-    return result;
+  public boolean verificarPalavra(String palavra) {
+    return palavasUnicas.contains(palavra);
   }
 
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj)
-      return true;
-    if (obj == null)
-      return false;
-    if (getClass() != obj.getClass())
-      return false;
-    ConjuntoPalavrasUnicas other = (ConjuntoPalavrasUnicas) obj;
-    if (palavasUnicas == null) {
-      if (other.palavasUnicas != null)
-        return false;
-    } else if (!palavasUnicas.equals(other.palavasUnicas))
-      return false;
-    return true;
-  }
 
   @Override
   public String toString() {
