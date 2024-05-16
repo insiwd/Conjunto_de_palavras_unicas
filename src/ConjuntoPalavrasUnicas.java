6cc869c1594785cd34f86bd0d52e1fd6a3287a22
ruan <ruan.cavalcante505@gmail.com>
2024-05-16 09:47:12 -0300

    feat: hashcode and equals

--- src/ConjuntoPalavrasUnicas.java
+++ src/ConjuntoPalavrasUnicas.java
@@ -24,6 +24,31 @@ public class ConjuntoPalavrasUnicas {
   }
 
   @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((palavasUnicas == null) ? 0 : palavasUnicas.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    ConjuntoPalavrasUnicas other = (ConjuntoPalavrasUnicas) obj;
+    if (palavasUnicas == null) {
+      if (other.palavasUnicas != null)
+        return false;
+    } else if (!palavasUnicas.equals(other.palavasUnicas))
+      return false;
+    return true;
+  }
+
+  @Override
   public String toString() {
     return "palavasUnicas = " + palavasUnicas;
   }
